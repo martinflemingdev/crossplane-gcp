6bd6f431e59b379c5233bfd551e938142b3ec94e
Martin Fleming <martinfleming@pop-os.localdomain>
2023-08-10 11:36:29 -0400

    working KMS objects

--- scripts/crossplane.sh
+++ scripts/crossplane.sh
@@ -1,12 +1,11 @@
-kubectl create namespace crossplane-system
+#!/usr/bin/bash
 
+kubectl create namespace crossplane-system
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 helm repo update
 
 helm install crossplane --namespace crossplane-system crossplane-stable/crossplane
-
 helm list -n crossplane-system
-
 kubectl get all -n crossplane-system
 
 curl -sL https://raw.githubusercontent.com/crossplane/crossplane/master/install.sh | sh
