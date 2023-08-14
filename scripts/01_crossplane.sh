05856275cca45b72b920e21afe60ee6ad7d35276
Martin Fleming <martinfleming@pop-os.localdomain>
2023-08-14 14:45:38 -0400

    adding storage and script comments

--- scripts/01_crossplane.sh
+++ scripts/01_crossplane.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+# CROSSPLANE APP
 kubectl create namespace crossplane-system
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 helm repo update
@@ -8,4 +9,7 @@ helm install crossplane --namespace crossplane-system crossplane-stable/crosspla
 helm list -n crossplane-system
 kubectl get all -n crossplane-system
 
+# CROSSPLANE CLI
+echo installing crossplane CLI to /usr/local/bin
 curl -sL https://raw.githubusercontent.com/crossplane/crossplane/master/install.sh | sh
+sudo mv kubectl-crossplane /usr/local/bin
