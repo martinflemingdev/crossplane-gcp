0c716a665fba260aa138c94f71d0063e66010771
Martin Fleming <martinfleming@pop-os.localdomain>
2023-08-17 13:53:51 -0400

    adding different example

--- resources/Dataflow/classic-template-walkthru/myWordCount.py
+++ resources/Dataflow/classic-template-walkthru/myWordCount.py
@@ -1,25 +1,14 @@
+# from https://medium.com/swlh/apache-beam-google-cloud-dataflow-and-creating-custom-templates-using-python-c666c151b4bc
+
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.options.value_provider import StaticValueProvider
-from apache_beam.io import WriteToText, ReadFromText
 
-class WordcountOptions(PipelineOptions):
-    @classmethod
-    def _add_argparse_args(cls, parser):
-        # Use add_value_provider_argument for arguments to be templatable
-        # Use add_argument as usual for non-templatable arguments
-        parser.add_value_provider_argument(
-            '--input',
-            default='gs://dataflow-samples/shakespeare/kinglear.txt',
-            help='Path of the file to read from')
-        parser.add_argument(
-            '--output',
-            required=True,
-            help='Output file to write results to.')
-        
-
-pipeline_options = PipelineOptions(['--output', 'some/output_path'])
-p = beam.Pipeline(options=pipeline_options)
-
-wordcount_options = pipeline_options.view_as(WordcountOptions)
-lines = p | 'read' >> ReadFromText(wordcount_options.input)
+with beam.Pipeline(options=PipelineOptions()) as p:
+    lines = p | 'Creating PCollection' >> beam.Create(['Hello', 'Hello Good Morning', 'GoodBye'])
+    counts = (
+        lines
+        | 'Tokenizing' >> (beam.FlatMap(lambda x: x.split(' ')))
+        | 'Pairing With One' >> beam.Map(lambda x: (x, 1))
+        | 'GroupbyKey And Sum' >> beam.CombinePerKey(sum)
+        | 'Printing' >> beam.ParDo(lambda x: print(x[0], x[1]))
+        )
